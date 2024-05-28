23be8ae9104928c7689d5f85c63881b52ab6375c
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-28 11:34:05 +0200

    javas etc

---
+++ src/assets/ex1/app.js
@@ -0,0 +1,11 @@
+console.log("connected");
+
+// Renderiza un h1 en navegador
+//
+// dado que me encuentro en la página html
+//cuando se renderiza
+// entonces veo el titulo Hello JavaScript
+//
+
+//const hello = "Hello JavaScript"
+//const divApp = document.querySelector('app')
