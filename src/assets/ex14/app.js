64568ca7d43df73a1a346bfccdc81ffdd3142c86
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-06-08 21:59:15 +0200

    all the exercises done

---
+++ src/assets/ex14/app.js
@@ -0,0 +1,15 @@
+function encontrarDivisoresComunes() {
+    let num1 = document.getElementById('number1').value;
+    let num2 = document.getElementById('number2').value;
+    num1 = parseInt(num1, 10);
+    num2 = parseInt(num2, 10);
+    let divisoresComunes = [];
+  
+    for (let i = 1; i <= Math.min(num1, num2); i++) {
+      if (num1 % i == 0 && num2 % i == 0) {
+        divisoresComunes.push(i);
+      }
+    }
+  
+    document.getElementById('resultado').innerHTML = 'Divisores comunes: ' + divisoresComunes.join(', ');
+  }
---
+++ src/assets/ex15/app.js
@@ -0,0 +1,20 @@
+function verificarPrimo() {
+    let numero = document.getElementById('number').value;
+    numero = parseInt(numero, 10);
+    if (numero <= 1) {
+        document.getElementById('resultado').innerHTML = numero + ' no es un número primo.';
+        return;
+    }
+
+    if (numero === 2) {
+        document.getElementById('resultado').innerHTML = '2 es un número primo.';
+        return;
+    }
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            document.getElementById('resultado').innerHTML = numero + ' no es un número primo.';
+            return;
+        }
+    }
+    document.getElementById('resultado').innerHTML = numero + ' es un número primo.';
+}
